1b4dccd38d25827070778a405db08fb25af65b73
asakura89 <flamboyan3no1a@gmail.com>
2020-10-10 10:04:31 +0700

    [New] Stopwatch

--- Dyandra.View/DyandraWindow.xaml.cs
+++ Dyandra.View/DyandraWindow.xaml.cs
@@ -1,20 +1,53 @@
 using System;
 using System.Timers;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Dyandra.View {
     public partial class DyandraWindow : UserControl {
+        const Int32 TimerInterval = 50;
+        const Double TimerIntervalDouble = 50d;
+
+        Timer stopwatchTimer = new Timer(TimerInterval);
+        TimeSpan elapsed = new TimeSpan();
+
         public DyandraWindow() {
             InitializeComponent();
 
-            var timer = new Timer(500);
+            var timer = new Timer(TimerInterval);
             timer.Elapsed += TimerOnElapsed;
-            timer.Enabled = true;
+            timer.Start();
+
+            stopwatchTimer.Elapsed += StopWatchTimerOnElapsed;
         }
 
         void TimerOnElapsed(Object sender, ElapsedEventArgs e) {
             String time = DateTime.Now.ToString("h:mm:ss tt zz");
             Dispatcher.Invoke(() => TxtTime.Text = time);
+        }
+
+        void StopWatchTimerOnElapsed(Object sender, ElapsedEventArgs e) {
+            elapsed = TimeSpan.FromMilliseconds(elapsed.TotalMilliseconds + TimerIntervalDouble);
+            Dispatcher.Invoke(UpdateStopwatchText);
+        }
+
+        void UpdateStopwatchText() =>
+            TxtStopwatch.Text = $"{elapsed.Hours:00}:{elapsed.Minutes:00}:{elapsed.Seconds:00}.{elapsed.Milliseconds:000}";
+
+        void StartButton_Click(Object sender, RoutedEventArgs e) {
+            if (!stopwatchTimer.Enabled)
+                stopwatchTimer.Start();
+        }
+
+        void StopButton_Click(Object sender, RoutedEventArgs e) {
+            if (stopwatchTimer.Enabled)
+                stopwatchTimer.Stop();
+        }
+
+        void ResetButton_Click(Object sender, RoutedEventArgs e) {
+            stopwatchTimer.Stop();
+            elapsed = new TimeSpan();
+            UpdateStopwatchText();
         }
     }
 }
